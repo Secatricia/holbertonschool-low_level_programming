f7cbbd5dabc7789b133f5f48e827075e27dc572f
Secatricia <3560@holbertonschool.com>
2021-10-01 13:02:03 +0000

    Ajout exo 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ecrire l'alphabet en minuscule et majuscule
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    int nombre = 0;
+    while (nombre < 10)
+    {
+        putchar(nombre);
+        nombre++;
+    }
+    putchar('\n');
+    return(0);
+}
