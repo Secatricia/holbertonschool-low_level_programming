4093820e67c564dca720ec81786ab1526845dfc0
Secatricia <3560@holbertonschool.com>
2021-10-05 16:01:06 +0000

    Ajout exo 11 correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * print_to_98 - check the code
  *
- * Return: Always 0.
+ * @n: variable
  */
 void print_to_98(int n)
 {
-    while (n != 98)
-    {
-    if (n < 98)
-    {
-        printf("%d, ", n);
-        n++;
-    }
-    else if (n > 98)
-    {
-        printf("%d, ", n);
-        n--;
-    }
-    }
-    
-    printf("%d, 98 \n", n);
+	while (n != 98)
+	{
+	if (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	else if (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	}
+	printf("%d, 98 \n", n);
 }
