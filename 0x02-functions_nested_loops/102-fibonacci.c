706a5396138e9739414eaaca42e9783dbe6af63d
Secatricia <3560@holbertonschool.com>
2021-10-05 20:11:37 +0000

    Ajout exo 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - addition somme multiple
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+int i = 0;
+int b = 1;
+int a = 1;
+
+    printf("%d", b);
+while (i < 100)
+{
+    printf("%d ,", a);
+    a += b;
+    i++;
+}
+printf("%d\n", a);
+
+	return (0);
+}
+ 
