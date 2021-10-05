fb7d8883ed139f6ea78a5a1f870de391d47dea64
Secatricia <3560@holbertonschool.com>
2021-10-05 15:03:41 +0000

    Ajout exo 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - ajout 2 int
+ *
+ *@a: variable
+ *
+ *@b: Variable
+ *
+ * Return: table
+ */
+
+int add(int a, int b)
+{
+    int n = a + b;
+    return (n);
+}
