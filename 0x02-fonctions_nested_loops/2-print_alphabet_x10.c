63b132bd0a336e5327b13f37c9816a9e5c223446
Secatricia <3560@holbertonschool.com>
2021-10-05 08:43:19 +0000

    Ajout exo 2

---
+++ 0x02-fonctions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - ecrire l'alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char lettre = 'a';
+	int i = 0;
+
+	while (i < 11)
+	{
+		while (lettre <= 'z')
+		{
+			_putchar(lettre);
+			lettre++;
+		}
+	_putchar('\n');
+	lettre = 'a';
+	i++;
+	}
+}
