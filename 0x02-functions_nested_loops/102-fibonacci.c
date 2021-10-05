e7ca24fceca214c83d4b0fe0c29c31eb59459dad
Secatricia <3560@holbertonschool.com>
2021-10-05 20:36:36 +0000

    Ajout exo 102 correction

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,16 +12,18 @@ int main(void)
 int i = 0;
 int b = 1;
 int a = 1;
-int c = 1;
 
     printf("%d", b);
 while (i < 100)
 {
-    printf("%d ,", c);
-    c = a + b;
+    printf("%d ,", a);
+    a += b;
+    printf("%d", a);
+    b += a;
+    printf("%d", b);
     i++;
 }
-printf("%d\n", c);
+printf("%d\n", b);
 
 	return (0);
 }
