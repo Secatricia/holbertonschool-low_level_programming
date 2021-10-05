73f6db945e5dcf88f0f98e96ced7a9273280079f
Secatricia <3560@holbertonschool.com>
2021-10-05 08:03:56 +0000

    Ajout exo 0

---
+++ 0x02-fonctions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char afficher[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(afficher[i]);
+		_putchar('\n');
+	}
+	return (0);
+}
