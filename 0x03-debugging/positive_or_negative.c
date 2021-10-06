561cd48c6faf2d052c507593a87ded89023db040
Secatricia <3560@holbertonschool.com>
2021-10-06 11:09:26 +0000

    Ajout correction 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * positive_or_negative - is number is positive or negative
+ *
+ * @i: variable
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+	printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+	printf("%d is negative\n", i);
+}
+else
+{
+	printf("%d is zero\n", i);
+}
+}
