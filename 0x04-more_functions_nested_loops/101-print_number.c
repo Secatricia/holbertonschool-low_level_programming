ac01303c1f94948541c86ec1fcd9ad9642472a4f
Secatricia <3560@holbertonschool.com>
2021-10-07 19:24:54 +0000

    Ajout correction 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,25 +3,27 @@
 /**
  * print_number - afficher des nombres entiers
  *
- * @n: number to print
+ * @n: variable
  */
-
 void print_number(int n)
 {
-
-	unsigned int i = 0, j = 1;
+	unsigned int i = 0;
 	unsigned int number_in_n;
+	unsigned int j = 1;
 
 	if (n == 0)
 		_putchar(48);
 	else
 	{
 		if (n < 0)
-			_putchar('-'), n = -n, number_in_n = n;
+		{
+			_putchar('-'), n = n * (-1);
+		}
+		number_in_n = n;
 		while (number_in_n > 0)
 		{
-			number_in_n = number_in_n / 10;
 			i++;
+			number_in_n = number_in_n / 10;
 		}
 		while (i > 1)
 		{
