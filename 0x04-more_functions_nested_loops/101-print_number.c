38934bda8ce814ed7ffee9d324350592663943db
Secatricia <3560@holbertonschool.com>
2021-10-07 16:15:42 +0000

    Ajout exo 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - afficher un nombre entier
+ *
+ *@n: variable
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n, _putchar('-');
+	}
+	if (n >= 10000 && n <= 32767)
+	{
+		_putchar((n / 10000) + 48), _putchar(((n / 1000) % 10) + 48);
+		_putchar(((n / 100) % 10) + 48), _putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 1000 && n <= 9999)
+	{
+		_putchar((n / 1000) + 48), _putchar(((n / 100) % 10) + 48);
+		_putchar(((n / 10) % 10) + 48), _putchar((n % 10) + 48);
+	}
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar((n / 100) + 48), _putchar(((n / 10) % 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	else if (n >= 10 && n <= 99)
+	{
+		_putchar((n / 10) + 48), _putchar((n % 10) + 48);
+	}
+	else if (n < 10 && n >= 0)
+	{
+		_putchar(n + 48);
+	}
+}
