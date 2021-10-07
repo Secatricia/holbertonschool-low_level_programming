9b8dd46e1be983917c7baea618d021729d05df19
Secatricia <3560@holbertonschool.com>
2021-10-07 12:02:09 +0000

    Ajout exo 9 correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,24 @@ int main(void)
 
 	while (i < 101)
 	{
-        if(i % 5 != 0 && i % 3 != 0)
-        {
-            printf("%d ", a);
-        }
-        else if (i % 5 == 0 && i % 3 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        
-        a++;
-        i++;
+		if (i % 5 != 0 && i % 3 != 0)
+		{
+			printf("%d ", a);
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		a++;
+		i++;
 	}
-    return (0);
+	return (0);
 }
