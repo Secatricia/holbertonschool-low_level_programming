e8d28ed52361128330a381c15356e0c6152f8190
Secatricia <3560@holbertonschool.com>
2021-10-07 16:32:07 +0000

    Correction 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,32 +8,33 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	long int num = n;
+
+	if (num < 0)
 	{
-		n = -n, _putchar('-');
+		num = -num, _putchar('-');
 	}
-	if (n >= 10000)
+	if (num >= 10000 && num <= 32767)
 	{
-		_putchar((n / 10000) + 48), _putchar(((n / 1000) % 10) + 48);
-		_putchar(((n / 100) % 10) + 48), _putchar(((n / 10) % 10) + 48);
-		_putchar((n % 10) + 48);
+		_putchar((num / 10000) + 48), _putchar(((num / 1000) % 10) + 48);
+		_putchar(((num / 100) % 10) + 48), _putchar(((num / 10) % 10) + 48);
+		_putchar((num % 10) + 48);
 	}
-	else if (n >= 1000 && n <= 9999)
+	else if (num >= 1000 && n <= 9999)
 	{
-		_putchar((n / 1000) + 48), _putchar(((n / 100) % 10) + 48);
-		_putchar(((n / 10) % 10) + 48), _putchar((n % 10) + 48);
+		_putchar((num / 1000) + 48), _putchar(((num / 100) % 10) + 48);
+		_putchar(((num / 10) % 10) + 48), _putchar((num % 10) + 48);
 	}
-	else if (n >= 100 && n <= 999)
+	else if (num >= 100 && num <= 999)
 	{
-		_putchar((n / 100) + 48), _putchar(((n / 10) % 10) + 48);
-		_putchar((n % 10) + 48);
+		_putchar((num / 100) + 48), _putchar(((num / 10) % 10) + 48);
+		_putchar((num % 10) + 48);
 	}
-	else if (n >= 10 && n <= 99)
+	else if (num >= 10 && num <= 99)
 	{
-		_putchar((n / 10) + 48), _putchar((n % 10) + 48);
+		_putchar((num / 10) + 48), _putchar((num % 10) + 48);
 	}
-	else if (n < 10 && n >= 0)
+	else if (num < 10 && num >= 0)
 	{
-		_putchar(n + 48);
-	}
-}
+		_putchar(num + 48);
+}}
