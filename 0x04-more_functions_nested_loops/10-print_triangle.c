dcc03876b47484af3a21763095f12338c2021592
Secatricia <3560@holbertonschool.com>
2021-10-07 12:59:34 +0000

    Ajout exo 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - faire des figures
+ *
+ *@size: variable
+ */
+void print_triangle(int size)
+{
+	int i = 1;
+	int j;
+
+	while (i <= size)
+	{
+		j = 0;
+
+		while (j <= size - i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		while (j <= size)
+		{
+			_putchar('#');
+			j++;
+		}
+	_putchar('\n');
+	i++;
+	}
+}
