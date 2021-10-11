c380ca793b0cb2ab07335274f0574047faf6d031
Secatricia <3560@holbertonschool.com>
2021-10-11 12:27:36 +0000

    Ajout exo 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - met une chaine à l'envers
+ *
+ * @s: variable
+ */
+void rev_string(char *s)
+{
+	int size = 0;
+	int j = 0;
+	int valeur_de_j;
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	while (j <= size)
+	{
+		valeur_de_j = s[j];
+		s[j] = s[size - 1];
+		s[size - 1] = valeur_de_j;
+
+		size--;
+		j++;
+	}
+}
