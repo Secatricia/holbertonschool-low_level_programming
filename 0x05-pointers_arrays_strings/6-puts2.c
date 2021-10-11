734496d6f484ae561bb76e6499163a80069416f8
Secatricia <3560@holbertonschool.com>
2021-10-11 12:49:44 +0000

    Ajout exo 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - Parcour une chaine de 2 en 2
+ *
+ * @str: variable
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
