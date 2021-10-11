51afcf7ede61d602d4554c730647da3d8116894e
Secatricia <3560@holbertonschool.com>
2021-10-11 08:59:20 +0000

    Ajout exo 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ *
+ * @s: variable
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+    while (*s != '\0')
+    {
+        s++;
+        i++;
+    }
+    return (i);
+}
