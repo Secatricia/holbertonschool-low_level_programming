63b8a7558de836b384888d1f54e0dcf1f6e17f99
Secatricia <3560@holbertonschool.com>
2021-10-11 09:58:26 +0000

    Ajout exo 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Affiche une chaine à l'envers
+ *
+ * @s: variable
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j = i;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (j <= i)
+	{
+		_putchar(*s);
+		s--;
+		j++;
+	}
+	_putchar('\n');
+}
