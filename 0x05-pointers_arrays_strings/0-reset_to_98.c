f07f4bb0eb30029fb56990047d79c05cb0c6a797
Secatricia <3560@holbertonschool.com>
2021-10-11 07:42:58 +0000

    Ajout correction exo 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * reset_to_98 - check the code
  *
- * Return: Always 0.
+ * @n: variable
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
