d0b8f340f3b705800a5290969adae9b617ef03f9
Secatricia <3560@holbertonschool.com>
2021-10-14 11:03:57 +0000

    Ajout correction exo 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0 ; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
 		continue;
 	}
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j < n && src[j]; j++, i++)
 	{
 		dest[i] = src[j];
 	}
