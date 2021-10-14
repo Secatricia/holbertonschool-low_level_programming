1bc33c95554a7db67b1a39a9ea9eb0e45fe3b155
Secatricia <3560@holbertonschool.com>
2021-10-14 11:11:00 +0000

    Ajout correction exercice 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,9 +10,10 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	for (i = 0; n[i] != '\0' && n[i] < 123 && n[i] > 96; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		n[i] = n[i] - 32;
+		if (n[i] < 123 && n[i] > 96)
+			n[i] = n[i] - 32;
 	}
 	return (n);
 }
