ba18f8ed090f8c028f5eeb474e4bfc7296fea42f
Secatricia <3560@holbertonschool.com>
2021-10-14 19:48:27 +0000

    Ajout exo 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - afficher un nombre avec _putchar
+ * @n: variable
+ */
+void print_number(int n)
+{
+	int nombre;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nombre = -n;
+	}
+	else
+	{
+		nombre = n;
+	}
+	if (nombre / 10)
+	{
+		print_number(nombre / 10);
+	}
+	_putchar((nombre % 10) + 48);
+}
