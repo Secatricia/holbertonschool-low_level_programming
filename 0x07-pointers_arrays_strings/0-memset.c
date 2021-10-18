954b5116e265b7e12b83ae83d0626091f7b9921c
Secatricia <3560@holbertonschool.com>
2021-10-18 10:25:00 +0000

    Correction boucle for

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 
 /**
- * memset - La fonction _memset() remplit les premiers n octets
- * de la zone mémoire pointée par s avec la constante byte b 
+ * _memset - La fonction remplit les premiers n octets
+ * de la zone mémoire pointée par s avec la constante byte b
  * Renvoie un pointeur vers la zone de mémoire s
  * @s: variable
  * @b: variable
  * @n: variable
- * Return: ()
+ * Return: (s)
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-	for (i = 0; i < n - 1; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
