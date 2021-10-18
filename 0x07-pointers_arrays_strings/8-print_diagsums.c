7aaab481e12e222719ad560719ea90cdbd9d3e34
Secatricia <3560@holbertonschool.com>
2021-10-18 16:32:21 +0000

    Betty exo 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - Write a function that prints the chessboard.
- * a string for any of a set of bytes.
+ * print_diagsums - Write a function that prints the sum
+ * of the two diagonals of a square matrix of integers.
+ * @size: variable
  * @a: variable
  */
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int somme1;
-	int somme2;
+	int somme1 = 0;
+	int somme2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		somme1 = somme1 + (*((a) + (size * i) + i));
-		somme2 = somme2 + (*((a) + (size * i) - (i +1) + size));
+		somme2 = somme2 + (*((a) + (size * i) - (i + 1) + size));
 	}
 	printf("%d, %d\n", somme1, somme2);
 }
