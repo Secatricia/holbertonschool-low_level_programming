6cf97046bab0c515c1358f0ea7875198f2caf353
Secatricia <3560@holbertonschool.com>
2021-10-18 13:07:01 +0000

    Ajout exo 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - Write a function that searches a string for any of a set of bytes.
+ *
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0' && accept[j] != s[i]; j++)
+		{
+			continue;
+		}
+		if (s[i] == accept[j])
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == accept[j])
+	{
+		return (s + i);
+	}
+	else
+	{
+		return (0);
+	}
+}
