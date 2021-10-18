21264e10c40b1c55f81cbeb76f9d5a39a2dcbfc4
Secatricia <3560@holbertonschool.com>
2021-10-18 12:20:02 +0000

    Ajout exercice 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - Ecrire une fonction qui obtient la longueur d’un préfixe.
+ * @s: variable
+ * @accept: variable
+ * Return: (k)
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0' && accept[j] != s[i]; j++)
+		{
+			continue;
+		}
+		if (accept[j] != s[i])
+		{
+			k += 1;
+		}
+	}
+	return (k);
+}
