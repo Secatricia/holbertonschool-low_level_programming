74fbeccfcfb3c271775cb0e4d11c681d44588229
Secatricia <3560@holbertonschool.com>
2021-10-18 18:37:53 +0000

    Ajout exo 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - Ecrire une fonction
+ * qui définit la valeur d’un pointeur à un char.
+ * @s: variable
+ * @to: variable
+ */
+
+void set_string(char **s, char *to)
+{
+
+}
