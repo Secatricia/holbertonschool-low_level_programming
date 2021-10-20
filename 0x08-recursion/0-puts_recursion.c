db17ee2ed359f08a46aace0d4cebc977d0f11b1d
Secatricia <3560@holbertonschool.com>
2021-10-20 09:10:15 +0000

    Ajout exo 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Write a function that prints a string,
+ * followed by a new line.
+ * @s: variable
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s)
+	{
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
