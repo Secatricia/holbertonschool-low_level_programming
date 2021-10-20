c256488e10ab024dd635debcdddcd02d98527826
Secatricia <3560@holbertonschool.com>
2021-10-20 10:47:03 +0000

    Ajout exo 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Write a function
+ * that prints a string in reverse.
+ * @s: variable
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+		s--;
+	}
+	else
+	{
+		;
+	}
+}
