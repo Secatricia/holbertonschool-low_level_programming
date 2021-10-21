02aa3834adc6c49baed48f14bf7d2cd442a84c40
Secatricia <3560@holbertonschool.com>
2021-10-21 20:28:31 +0000

    Ajout exo 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,10 +7,29 @@
  * otherwise return 0.
  *@s1: variable
  *@s2: variable
- * Return: Always 1.
+ * Return: (1, (wildcmp(s1, s2 + 1),
+ * (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2) ou 0)
  */
 int wildcmp(char *s1, char *s2)
 {
-	*s1 = *s2 / 2;
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+		}
+	}
 	return (0);
 }
