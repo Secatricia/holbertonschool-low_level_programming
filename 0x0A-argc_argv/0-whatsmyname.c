bda89993bb89796abb7c48c142ace4997f3db8ee
Secatricia <3560@holbertonschool.com>
2021-10-22 11:27:00 +0000

    Ajout _putchar

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
-{ 
-	printf ("%s\n", argv[0]);
+/**
+ * main - Write a program that prints its name,
+ * followed by a new line.
+ * @argc: variable
+ * @argv: variable
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
 	(void)argc;
-	return (0); 
+	return (0);
 }
