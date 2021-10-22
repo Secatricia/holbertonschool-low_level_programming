26e76a79affe10fb65ba51dd5247697ed72e0a78
Secatricia <3560@holbertonschool.com>
2021-10-22 12:33:32 +0000

    Ajout exo 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argv: variable
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int j = 0, somme = 0;
+	int i = 1;
+
+	while (i < argc)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		somme += atoi(argv[i]);
+		j++;
+	}
+	printf("%d\n", somme);
+
+	return (0);
+}
