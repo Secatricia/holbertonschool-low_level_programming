1def2ae56fcad95932898e2c9bc38f8577ca011b
Secatricia <3560@holbertonschool.com>
2021-10-26 13:02:30 +0000

    Ajout fonction strcpy et strlen dans strdup exo 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,6 +3,49 @@
 #include "main.h"
 
 /**
+ * _strcpy - copier coller
+ * @src: variable
+ * @dest: variable
+ * Return: (dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int size = 0;
+	int j = 0;
+
+	while (src[size] != '\0')
+	{
+		size++;
+	}
+
+	while (j <= size)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - Compte le nombre de caractere d'une chaine
+ *
+ * @s: variable
+ *
+ * Return: (i)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * _strdup - Write a function that returns a pointer
  * to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter.
