59b424702c03cc2ba0d5a3b991df6bb605f590e5
Secatricia <3560@holbertonschool.com>
2021-10-26 18:59:45 +0000

    Ajout exo 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,70 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strlen - Compte le nombre de caractere d'une chaine
- *
- * @s: variable
- *
- * Return: (i)
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-	return (i);
-}
-
-/**
- * _strcat - concatene
- * @src: variable
- * @dest: variable
- * Return: (dest)
- */
-char *_strcat(char *dest, char *src)
-{
-	int i;
-	int j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
-	for (j = 0; src[j] != '\0'; j++, i++)
-	{
-		dest[i] = src[j];
-	}
-	dest[i] = src[j];
-	return (dest);
-}
-
-/**
- * _strdup - Write a function that concatenates two strings
- * @s1: variable
- * @s2: variable
- * Return: (NULL ou )
+ * str_concat - Concatenate 2 string into a new one
+ * @s1: Str one
+ * @s2: Str two
+ * Return: NULL if probleme, pointer to new str else (succes)
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *conc = NULL;
+	int length1, length2, totalLenght, i;
 
-	if (s2 == NULL && s1 == NULL)
-	{
-		return (NULL);
-	}
-	s1 = malloc(sizeof(char) * _strlen(s2) + 1);
+	if (s1 == NULL)
+		length1 = 0;
+	else
+		length1 = _strlen(s1);
 
 	if (s2 == NULL)
-	{
-		return (s1);
-	}
-	conc = _strcat(s1, s2);
+		length2 = 0;
+	else
+		length2 = _strlen(s2);
 
+	totalLenght = length2 + length1;
+	conc = (char *)malloc(sizeof(char) * totalLenght + 1);
+
+	if (conc == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+		*(conc + i) = *(s1 + i);
+	for (i = 0; i < length2; i++)
+		*(conc + length1 + i) = *(s2 + i);
+	*(conc + length1 + length2) = '\0';
 	return (conc);
+}
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: String to manipulate.
+ * Return: The length of the inputed string.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
