ec1bf313ae9c7b36a3f9854414f82a28a143d66f
Secatricia <3560@holbertonschool.com>
2021-10-26 08:58:01 +0000

    Ajout exo 0 README.md 0-main.c main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create an array
+ * @size: variable
+ * @c: variable
+ * Return: (NULL ou )
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i = 0;
+	ar = malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		ar[i] = c;
+		i++;
+	}
+	return (ar);
+}
