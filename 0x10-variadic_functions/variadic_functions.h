bddc409dc4885e32c7228ee1470ff37c5aa3a4f5
Secatricia <3560@holbertonschool.com>
2021-11-04 22:16:48 +0000

    Ajout exo 3 et correction 1 et 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,15 +4,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct fonct
+/**
+ * struct searchtype - Struct searchtype
+ *
+ * @typeOf: The type we search
+ * @f: The function associated
+ */
+typedef struct whattype
 {
-	char *fc;
-	int (*f)(int a, int b);
-} op_t;
+	char *a;
+	void (*f)(va_list);
+} choice;
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
+
+void print_char(va_list listFormat);
+void print_integer(va_list listFormat);
+void print_float(va_list listFormat);
+void print_string(va_list listFormat);
 
 #endif /*VARIADIC_FUNCTIONS_H*/
