c5c0b075a107d0f6faabf53fd6a0bad71d78e3ac
Secatricia <3560@holbertonschool.com>
2021-11-12 21:28:09 +0000

    Ajout exo 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at
+ * the beginning of a list_t list
+ * @head: pointeur vers la premiere node
+ * @str: parametre à ajouter
+ * Return: nb_node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
+
+/**
+* _strlen - check the length of the string
+* Return: length of the string
+* @s: string
+*/
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * _strlen - Compte le nombre de caractere d'une chaine
+ *
+ * @s: variable
+ *
+ * Return: (i)
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
