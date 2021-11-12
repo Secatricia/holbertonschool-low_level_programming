685b3c7cea1bdc31543c08293159d622fa470e09
Secatricia <3560@holbertonschool.com>
2021-11-12 22:04:49 +0000

    Ajout exo 4

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * beforeTheMain - function that print before main
+ */
+
+void beforeTheMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
