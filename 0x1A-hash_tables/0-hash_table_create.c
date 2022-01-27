c0c1d565937778c54399db5f0522b6aa8c0cfd3a
Secatricia <3560@holbertonschool.com>
2022-01-27 14:13:14 +0000

    Add ex 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int a = 0; /*Initialise value*/
+
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	/*Add memory for struc*/
+
+	if (hash == NULL)
+	{
+		return NULL;
+	}
+
+	if (a < size)
+	{
+		(*hash).size = size;
+	}
+
+	hash->array = malloc(sizeof(hash_table_t) * size);
+	if (hash->array == NULL)
+	{
+		return NULL;
+	}
+
+	while (a < size)
+	{
+		(*hash).array[a] = NULL;
+		a++;
+	}
+	return (hash);
+}
