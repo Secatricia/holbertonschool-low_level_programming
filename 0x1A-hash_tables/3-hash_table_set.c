46e424fefad519dae5c0780507078865256ee641
Secatricia <3560@holbertonschool.com>
2022-01-28 18:10:13 +0000

    Incrementation ex 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,6 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 		array_idx = array_idx->next;
+		index += 1;
 	}
 	node->key = strdup(key);
 	node->value = strdup(value);
